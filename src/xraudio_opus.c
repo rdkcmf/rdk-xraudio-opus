b52a7968559da1d9b437dbb39e71b82859a65a43
Stephen Barrett <stephen.barrett@accenture.com>
2021-04-14 15:20:39 +0100

    Import of Comcast source (stable2)
    Source: https://gerrit.teamccp.com/rdk/components/generic/xraudio/opus
    SHA1: 3fc12be0849569c61fc2963a0f9a4c8f7266097f
    Change-Id: I3b7e57f2517b9acb47df17037f9e742a44a97608

---
+++ src/xraudio_opus.c
@@ -0,0 +1,210 @@
+/*
+##########################################################################
+# If not stated otherwise in this file or this component's LICENSE
+# file the following copyright and licenses apply:
+#
+# Copyright 2019 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+*/
+#include <stdlib.h>
+#include <rdkx_logger.h>
+#include <xraudio_opus.h>
+#include <xraudio_opus_version.h>
+#include <opus/opus.h>
+
+#define XRAUDIO_OPUS_IDENTIFIER (0x378D6F5A)
+
+#define XRAUDIO_OPUS_HEADER_LENGTH (1)
+#define XRAUDIO_OPUS_CMD_ID_BEGIN  (0x20)
+#define XRAUDIO_OPUS_CMD_ID_END    (0x3F)
+
+typedef struct {
+   uint32_t     identifier;
+   OpusDecoder *decoder;
+   uint8_t      cmd_id_next;
+} xraudio_opus_obj_t;
+
+static bool xraudio_opus_obj_is_valid(xraudio_opus_obj_t *obj);
+static inline void xraudio_opus_cmd_id_inc(uint8_t *cmd_id);
+
+void xraudio_opus_version(const char **name, const char **version, const char **branch, const char **commit_id) {
+   if(name != NULL) {
+      *name      = "xraudio-opus";
+   }
+   if(version != NULL) {
+      *version   = XRAUDIO_OPUS_VERSION;
+   }
+   if(branch != NULL) {
+      *branch    = XRAUDIO_OPUS_BRANCH;
+   }
+   if(commit_id != NULL) {
+      *commit_id = XRAUDIO_OPUS_COMMIT_ID;
+   }
+}
+
+bool xraudio_opus_obj_is_valid(xraudio_opus_obj_t *obj) {
+   if(obj != NULL && obj->identifier == XRAUDIO_OPUS_IDENTIFIER) {
+      return(true);
+   }
+   return(false);
+}
+
+xraudio_opus_object_t xraudio_opus_create(void) {
+   
+   xraudio_opus_obj_t *obj = (xraudio_opus_obj_t *)malloc(sizeof(xraudio_opus_obj_t) + opus_decoder_get_size(1));
+
+   if(obj == NULL) {
+      XLOGD_ERROR("Out of memory");
+      return(NULL);
+   }
+   obj->identifier  = XRAUDIO_OPUS_IDENTIFIER;
+   obj->decoder     = (OpusDecoder *)&obj[1];
+   obj->cmd_id_next = XRAUDIO_OPUS_CMD_ID_BEGIN;
+
+   int rc = opus_decoder_init(obj->decoder, 16000, 1);
+   if(rc != OPUS_OK) {
+      XLOGD_ERROR("unable to create opus decoder <%d>", rc);
+      free(obj);
+      obj = NULL;
+   }
+
+   return(obj);
+}
+
+void xraudio_opus_cmd_id_inc(uint8_t *cmd_id) {
+   (*cmd_id)++;
+   if(*cmd_id > XRAUDIO_OPUS_CMD_ID_END) {
+      *cmd_id = XRAUDIO_OPUS_CMD_ID_BEGIN;
+   }
+}
+
+int32_t xraudio_opus_deframe(xraudio_opus_object_t object, uint8_t *inbuf, uint32_t inlen) {
+   xraudio_opus_obj_t *obj = (xraudio_opus_obj_t *)object;
+   if(!xraudio_opus_obj_is_valid(obj)) {
+      XLOGD_ERROR("invalid object");
+      return(-1);
+   }
+   if(inbuf == NULL) {
+      XLOGD_ERROR("invalid params");
+      return(-1);
+   }
+
+   if(inlen < XRAUDIO_OPUS_HEADER_LENGTH + 1) {
+      XLOGD_ERROR("invalid inlen <%u>", inlen);
+      return(-1);
+   }
+
+   // Remove the RF4CE framing and return only the opus stream
+   uint8_t cmd_id = inbuf[0];
+   
+   if(cmd_id < XRAUDIO_OPUS_CMD_ID_BEGIN || cmd_id > XRAUDIO_OPUS_CMD_ID_END) {
+      XLOGD_ERROR("invalid cmd id <0x%02X>", cmd_id);
+      return(-1);
+   }
+   if(cmd_id != obj->cmd_id_next) {
+      XLOGD_ERROR("discontinuity cmd id <0x%02X> expected <0x%02X", cmd_id, obj->cmd_id_next);
+      obj->cmd_id_next = cmd_id;
+   }
+   
+   xraudio_opus_cmd_id_inc(&obj->cmd_id_next);
+   
+   // Shift the data to remove the header (would be nice to optimize this copy away later)
+   uint8_t *payload = &inbuf[XRAUDIO_OPUS_HEADER_LENGTH];
+   for(uint32_t i = XRAUDIO_OPUS_HEADER_LENGTH; i < inlen; i++) {
+      *inbuf = *payload;
+   }
+
+   return(inlen - XRAUDIO_OPUS_HEADER_LENGTH);
+}
+
+int32_t xraudio_opus_decode(xraudio_opus_object_t object, uint8_t *inbuf, uint32_t inlen, pcm_t *outbuf, uint32_t outlen) {
+   xraudio_opus_obj_t *obj = (xraudio_opus_obj_t *)object;
+   if(!xraudio_opus_obj_is_valid(obj)) {
+      XLOGD_ERROR("invalid object");
+      return(-1);
+   }
+   if(inbuf == NULL || outbuf == NULL) {
+      XLOGD_ERROR("invalid params");
+      return(-1);
+   }
+
+   if(inlen < XRAUDIO_OPUS_HEADER_LENGTH + 1) {
+      XLOGD_ERROR("invalid inlen <%u>", inlen);
+      return(-1);
+   }
+
+   // Process the RF4CE framing
+   uint8_t cmd_id = inbuf[0];
+   
+   if(cmd_id < XRAUDIO_OPUS_CMD_ID_BEGIN || cmd_id > XRAUDIO_OPUS_CMD_ID_END) {
+      XLOGD_ERROR("invalid cmd id <0x%02X>", cmd_id);
+      return(-1);
+   }
+   if(cmd_id != obj->cmd_id_next) {
+      XLOGD_ERROR("discontinuity cmd id <0x%02X> expected <0x%02X", cmd_id, obj->cmd_id_next);
+      obj->cmd_id_next = cmd_id;
+   }
+   
+   xraudio_opus_cmd_id_inc(&obj->cmd_id_next);
+   
+   int samples = opus_decode(obj->decoder, &inbuf[XRAUDIO_OPUS_HEADER_LENGTH], inlen - XRAUDIO_OPUS_HEADER_LENGTH, outbuf, outlen, 0);
+   if(samples < 0) {
+      XLOGD_ERROR("failed to decode opus frame <%d>.  cmd id <0x%02X> len <%u>", samples, cmd_id, inlen);
+      return(samples);
+   }
+   return(samples * sizeof(pcm_t));
+}
+
+bool xraudio_opus_stats(xraudio_opus_object_t object, xraudio_opus_stats_t *stats) {
+   xraudio_opus_obj_t *obj = (xraudio_opus_obj_t *)object;
+   if(!xraudio_opus_obj_is_valid(obj)) {
+      XLOGD_ERROR("invalid object");
+      return(false);
+   }
+   if(stats == NULL) {
+      XLOGD_ERROR("invalid param");
+      return(false);
+   }
+   
+   return(true);
+}
+
+bool xraudio_opus_reset(xraudio_opus_object_t object) {
+   xraudio_opus_obj_t *obj = (xraudio_opus_obj_t *)object;
+   if(!xraudio_opus_obj_is_valid(obj)) {
+      XLOGD_ERROR("invalid object");
+      return(false);
+   }
+   
+   obj->cmd_id_next = XRAUDIO_OPUS_CMD_ID_BEGIN;
+   
+   int rc = opus_decoder_ctl(obj->decoder, OPUS_RESET_STATE);
+   if(rc != OPUS_OK) {
+      XLOGD_ERROR("unable to reset opus decoder state <%d>", rc);
+      return(false);
+   }
+
+   return(true);
+}
+
+void xraudio_opus_destroy(xraudio_opus_object_t object) {
+   xraudio_opus_obj_t *obj = (xraudio_opus_obj_t *)object;
+   if(!xraudio_opus_obj_is_valid(obj)) {
+      XLOGD_ERROR("invalid object");
+      return;
+   }
+   obj->identifier = 0;
+   free(obj);
+}
---
+++ src/xraudio_opus.h
@@ -0,0 +1,58 @@
+/*
+##########################################################################
+# If not stated otherwise in this file or this component's LICENSE
+# file the following copyright and licenses apply:
+#
+# Copyright 2019 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+*/
+#ifndef __XRAUDIO_OPUS_H__
+#define __XRAUDIO_OPUS_H__
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef void *  xraudio_opus_object_t;
+typedef int16_t pcm_t;
+
+typedef struct {
+   uint16_t packet_total;
+   uint16_t packet_lost;
+   uint8_t  err_discont;
+   uint8_t  err_repeat;
+   uint8_t  err_cmd_id;
+   uint8_t  err_cmd_len;
+} xraudio_opus_stats_t;
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+void xraudio_opus_version(const char **name, const char **version, const char **branch, const char **commit_id);
+
+xraudio_opus_object_t xraudio_opus_create(void);
+
+int32_t xraudio_opus_deframe(xraudio_opus_object_t object, uint8_t *inbuf, uint32_t inlen);
+int32_t xraudio_opus_decode(xraudio_opus_object_t object, uint8_t *inbuf, uint32_t inlen, pcm_t *outbuf, uint32_t outlen);
+bool    xraudio_opus_stats(xraudio_opus_object_t object, xraudio_opus_stats_t *stats);
+bool    xraudio_opus_reset(xraudio_opus_object_t object);
+void    xraudio_opus_destroy(xraudio_opus_object_t object);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
